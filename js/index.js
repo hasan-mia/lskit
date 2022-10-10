d6a127f89c5526794fd95de39ae8828b20e9ca2b
Md. Hasan Mia <hasanrafi69@gmail.com>
2022-10-10 16:23:34 +0600

    slick added

---
+++ js/index.js
@@ -0,0 +1,34 @@
+// =====Trend Slider====
+$('.comment').slick({
+    centerMode: false,
+    dots: false,
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 3,
+    variableWidth: true,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                variableWidth: true,
+                centerPadding: '40px',
+                slidesToShow: 5,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                variableWidth: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+
+});
